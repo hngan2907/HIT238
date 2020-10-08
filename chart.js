018d58e64e9ace7ad89fb35bc1c599f435264039
hngan2907 <hngan2907@gmail.com>
2020-10-09 01:22:37 +0930

    move test to chart.js

--- chart.js
+++ chart.js
@@ -10,18 +10,46 @@ function createDate(days, months, years) {
         return date;
     }
 
+
+
 const threemonths = createDate(-2,0,0);
 const threemonthsformat = threemonths.getFullYear() + "-" + ("0"+(threemonths.getMonth()+1)).slice(-2) + "-" + ("0" + threemonths.getDate()).slice(-2);
 
 
 const dataURL3months = 'https://api.exchangerate.host/timeseries?start_date='+encodeURI(threemonthsformat)+'&end_date='+encodeURI(today)+'&base=USD&format=csv';
 
-document.getElementById("p1").innerHTML = dataURL3months;
 
 const datedata = [];
 const ratedata = [];
 
-
+$(function () {
+    $('#empcontainer').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Attendance Comparison'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar']
+        },
+        yAxis: {
+            title: {
+                text: 'Attendance (# of Days)'
+            }
+        },
+        credits:{
+            enabled: false
+        },
+        series: [{
+            name: 'Kamal',
+            data: [25, 30, 28]
+        }, {
+            name: 'Vikas',
+            data: [15, 22, 30]
+        }],
+    });
+}); 
 async function getAsync()
 {
   const response = await fetch(dataURL3months);
@@ -33,8 +61,10 @@ async function getAsync()
       const code = bb[1];
       const rate = bb[2];
       const nn = [date,code,rate];
+      const e = document.getElementById("id1");
+      const curr1 = e.options[e.selectedIndex].value;
       for (i=0; i<nn.length; i++) {
-        if (nn[1] === "ALL") {
+        if (nn[1] === curr1) {
           datedata.push(nn[0]);
           ratedata.push(nn[2]);
         }
