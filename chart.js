b13253f926615be3a4e8924ce88b811c86ac0b76
hngan2907 <hngan2907@gmail.com>
2020-10-13 00:14:32 +0930

    dynamic data and get value of period options

--- chart.js
+++ chart.js
@@ -1,77 +1,67 @@
-
 const now = new Date();
-const today = now.getFullYear() + "-" + ("0"+(now.getMonth()+1)).slice(-2) + "-" + ("0" + now.getDate()).slice(-2);
+const today = now.getFullYear() + "-" + ("0" + (now.getMonth() + 1)).slice(-2) + "-" + ("0" + now.getDate()).slice(-2);
 
 function createDate(days, months, years) {
-        var date = new Date();
-        date.setDate(date.getDate() + days);
-        date.setMonth(date.getMonth() + months);
-        date.setFullYear(date.getFullYear() + years);
-        return date;
-    }
+  var date = new Date();
+  date.setDate(date.getDate() + days);
+  date.setMonth(date.getMonth() + months);
+  date.setFullYear(date.getFullYear() + years);
+  return date;
+}
 
 
+const aweek = createDate(-7, 0, 0);
+const aweekformat = aweek.getFullYear() + "-" + ("0" + (aweek.getMonth() + 1)).slice(-2) + "-" + ("0" + aweek.getDate()).slice(-2);
+const threemonths = createDate(0, -3, 0);
+const threemonthsformat = threemonths.getFullYear() + "-" + ("0" + (threemonths.getMonth() + 1)).slice(-2) + "-" + ("0" + threemonths.getDate()).slice(-2);
+const ayear = createDate(0, 0, -1);
+const ayearformat = ayear.getFullYear() + "-" + ("0" + (ayear.getMonth() + 1)).slice(-2) + "-" + ("0" + ayear.getDate()).slice(-2);
 
-const threemonths = createDate(-2,0,0);
-const threemonthsformat = threemonths.getFullYear() + "-" + ("0"+(threemonths.getMonth()+1)).slice(-2) + "-" + ("0" + threemonths.getDate()).slice(-2);
+function getchartdata () {
+
+const e = document.getElementById("curr1");
+const curr1 = e.options[e.selectedIndex].value;
+const b = document.getElementById("curr2");
+const curr2 = b.options[b.selectedIndex].value;
+
+const d = document.getElementById("id2");
+const getper = d.options[d.selectedIndex].value;
+
+var dataURL;
+if (getper == 'aweek') {
+  dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(aweekformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
+} else if (getper == 'threemonths') {
+  dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(threemonthsformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
+} else if (getper == 'ayear') {
+  dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(ayearformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
+} else {
+  document.getElementById('urlerr').innerHTML = "Please choose the period of the chart";
+}
+
+console.log(dataURL)
 
 
-const dataURL3months = 'https://api.exchangerate.host/timeseries?start_date='+encodeURI(threemonthsformat)+'&end_date='+encodeURI(today)+'&base=USD&format=csv';
-
-
-const datedata = [];
-const ratedata = [];
-
-$(function () {
-    $('#empcontainer').highcharts({
-        chart: {
-            type: 'bar'
-        },
-        title: {
-            text: 'Attendance Comparison'
-        },
-        xAxis: {
-            categories: ['Jan', 'Feb', 'Mar']
-        },
-        yAxis: {
-            title: {
-                text: 'Attendance (# of Days)'
-            }
-        },
-        credits:{
-            enabled: false
-        },
-        series: [{
-            name: 'Kamal',
-            data: [25, 30, 28]
-        }, {
-            name: 'Vikas',
-            data: [15, 22, 30]
-        }],
-    });
-}); 
-async function getAsync()
-{
-  const response = await fetch(dataURL3months);
+async function getAsync() {
+  const response = await fetch(dataURL);
   const data = await response.text();
-    const rows = data.split('"\n"');
-    rows.forEach( row =>{
-      const bb = row.split('","');
-      const date = bb[0];
-      const code = bb[1];
-      const rate = bb[2];
-      const nn = [date,code,rate];
-      const e = document.getElementById("id1");
-      const curr1 = e.options[e.selectedIndex].value;
-      for (i=0; i<nn.length; i++) {
-        if (nn[1] === curr1) {
-          datedata.push(nn[0]);
-          ratedata.push(nn[2]);
-        }
-      }
-    });
+  const rows = data.split('"\n"');
+  const datedata = [];
+  const ratedata = [];
 
+  rows.forEach(row => {
+    const bb = row.split('","');
+    const date = bb[0];
+    const code = bb[1];
+    const rate = bb[2];
+    const nn = [date, code, rate];
 
+    if (nn[1] === curr2) {
+      datedata.push(nn[0]);
+      ratedata.push(nn[2]);
+
+    }
+  });
+  console.log(datedata, ratedata)
 }
 
 getAsync()
@@ -79,3 +69,5 @@ getAsync()
 
     console.log(nn)
   );
+
+}
