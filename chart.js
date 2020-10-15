59ba04e852353320080f7b65b6c1acc39753020f
hngan2907 <hngan2907@gmail.com>
2020-10-15 20:33:25 +0930

    draw the chart

--- chart.js
+++ chart.js
@@ -17,75 +17,93 @@ const threemonthsformat = threemonths.getFullYear() + "-" + ("0" + (threemonths.
 const ayear = createDate(0, 0, -1);
 const ayearformat = ayear.getFullYear() + "-" + ("0" + (ayear.getMonth() + 1)).slice(-2) + "-" + ("0" + ayear.getDate()).slice(-2);
 
+
+
+
+const datedata = [];
+const ratedata = [];
+const ratenumber = [];
+
+
 function getchartdata () {
+  const e = document.getElementById("curr1");
+  const curr1 = e.options[e.selectedIndex].value;
+  const b = document.getElementById("curr2");
+  const curr2 = b.options[b.selectedIndex].value;
 
-const e = document.getElementById("curr1");
-const curr1 = e.options[e.selectedIndex].value;
-const b = document.getElementById("curr2");
-const curr2 = b.options[b.selectedIndex].value;
+  const d = document.getElementById("id2");
+  const getper = d.options[d.selectedIndex].value;
 
-const d = document.getElementById("id2");
-const getper = d.options[d.selectedIndex].value;
+  var dataURL;
+  if (getper == 'aweek') {
+    dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(aweekformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
+  } else if (getper == 'threemonths') {
+    dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(threemonthsformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
+  } else if (getper == 'ayear') {
+    dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(ayearformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
+  } else {
+    document.getElementById('urlerr').innerHTML = "Please choose the period of the chart";
+  }
 
-var dataURL;
-if (getper == 'aweek') {
-  dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(aweekformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
-} else if (getper == 'threemonths') {
-  dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(threemonthsformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
-} else if (getper == 'ayear') {
-  dataURL = 'https://api.exchangerate.host/timeseries?start_date=' + encodeURI(ayearformat) + '&end_date=' + encodeURI(today) + '&base=' + encodeURI(curr1) + '&format=csv'
-} else {
-  document.getElementById('urlerr').innerHTML = "Please choose the period of the chart";
-}
-
-console.log(dataURL)
+  console.log(dataURL)
 
 
 async function getAsync() {
   const response = await fetch(dataURL);
   const data = await response.text();
   const rows = data.split('"\n"');
-  const datedata = [];
-  const ratedata = [];
+
 
   rows.forEach(row => {
     const bb = row.split('","');
-    const date = bb[0];
-    const code = bb[1];
-    const rate = bb[2];
-    const nn = [date, code, rate];
-
-    if (nn[1] === curr2) {
-      datedata.push(nn[0]);
-      ratedata.push(nn[2]);
-
+    if (bb[1] === curr2) {
+      datedata.push(bb[0]);
+      ratedata.push(bb[2]);
     }
   });
-  console.log(datedata, ratedata)
+  const try1 = [];
+  ratedata.forEach (mm => try1.push(mm.replace(",",".")));
+  const getnumber = try1.map(Number);
+getnumber.forEach (kk => ratenumber.push(kk));
+
+
 }
+
 
 getAsync()
-  .then(nn =>
+  .then(bb =>
 
-    console.log(nn)
+    console.log(bb)
   );
 
-}
 
-
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'line',
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'line',
   data: {
-    labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-    datasets: [{
-      label: 'apples',
-      data: [12, 19, 3, 17, 6, 3, 7],
-      backgroundColor: "rgba(153,255,51,0.4)"
-    }, {
-      label: 'oranges',
-      data: [2, 29, 5, 5, 2, 3, 10],
-      backgroundColor: "rgba(255,153,0,0.4)"
-    }]
+    labels: datedata,
+    datasets: [
+      {
+        label:curr2,
+        data: ratenumber,
+        borderColor: '#af90ca',
+        backgroundColor: '#af90ca',
+        fill: false,
+        lineTension: 0
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
+    }
   }
-});
+  });
+
+
+
+}
